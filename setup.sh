d7e87fb533ebcf84d07e3f515d04461e360a86e0
sammyjeng <samepic005@gmail.com>
2021-01-10 12:45:12 +0530

    cleanups

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ls ~/.shodan/api_key &>/dev/null
-if [[ $? -ne 0 ]]; then
+mycmd=$(ls ~/.shodan/api_key &>/dev/null)
+if [[ $mycmd -ne 0 ]]; then
     echo "This script will take care of installing shodan cli and and initiating it"
     echo ""
     echo "Create an account in shodan.io"
@@ -13,8 +13,8 @@ if [[ $? -ne 0 ]]; then
     echo ""
     sleep 1
     echo "Paste your shodan API KEY"
-    read -p "here : " AK
-    shodan init $AK #initiating shodan api
+    read -rp "here : " AK
+    shodan init "$AK" #initiating shodan api
     echo "It is all set up.!!"
 else
     echo "You are good to go!!"
